4dca22d55180bce7c98883cf626d9dd4c0fec355
David Vergona <dsvergona@gmail.com>
2017-12-20 12:14:49 -0800

    minor text fix

--- content/client/crud/module.js
+++ content/client/crud/module.js
@@ -1,10 +1,10 @@
 (function () {
 'use strict'
 
-angular.module('client.crud', ['ui.router', 'client.services','ui-bootstrap'])
+angular.module('client.crud', ['ui.router', 'client.services','ui.bootstrap'])
 angular.module('client.crud').config(RouteConfig)
 
-RouteConfig.$inject = ['$StateProvider']
+RouteConfig.$inject = ['$stateProvider']
 
 function RouteConfig($stateProvider){}
 
